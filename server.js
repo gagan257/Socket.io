dd913091a26d965fc31b006dd533b211adf88960
Gagan Arora <arorag2577@gmail.com>
2023-04-16 23:42:57 +0530

    first socket connection

--- server.js
+++ server.js
@@ -1,1 +1,17 @@
-// Hello sockets
+const http = require('http')
+const express = require('express')
+const app = express()
+const socketio = require('socket.io')
+
+const server = http.createServer(app)
+const io = socketio(server)
+
+io.on('connection',(socket)=>{
+    console.log('connected with socket id = ', socket.id)
+})
+
+app.use('/',express.static(__dirname + '/public'))
+
+server.listen(3344, ()=>{
+    console.log('started on http://localhost:3344')
+})
