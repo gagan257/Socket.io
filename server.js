6403f6107c3804a6d81036e4c7ae4c1b0943979e
Gagan Arora <arorag2577@gmail.com>
2023-04-22 12:11:36 +0530

    server to client message

--- server.js
+++ server.js
@@ -8,6 +8,14 @@ const io = socketio(server)
 
 io.on('connection',(socket)=>{
     console.log('connected with socket id = ', socket.id)
+
+    socket.on('boom', ()=>{
+        console.log('boom received from ', socket.id)
+    })
+
+    setInterval(() => {
+        socket.emit('whizz')
+    }, 2000);
 })
 
 
